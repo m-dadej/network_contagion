b9d099ba84dd6bcddb65ffd4eb8174953c4450b6
SquintRook <mateuszdadej@gmail.com>
2023-11-26 13:27:00 +0100

    warn

--- contagion_simulation.jl
+++ contagion_simulation.jl
@@ -11,7 +11,7 @@ include("optim_alloc_jump.jl")
 d = [(606/1.06), (807/1.5), (529/1.08), (211/0.7), (838/1.47), (296/0.63), (250/0.68), (428/2), (284/1.24), (40/0.94), (8.2/0.2), (252/1.74), (24/0.19), (111.1/1.03), (88.9/1.3), (51.8/0.42), (63/0.48), (111.1/1.65), (100/1.37), (11.6/0.15)] # rand(Normal(700, 100), N) # deposits
 e = [55.6, 90.0, 48.5, 53.0, 81.0, 53.0, 57.0, 48.0, 26.0, 43.0, 20.0, 23.0, 16.0, 10.0, 8.0, 5.0, 6.0, 10.0, 9.0, 9.0] #rand(Normal(50, 20), N) # equity
 
-n_sim = 10
+n_sim = 20
 σ_params = collect(-1.5:0.25:0)
 
 results = DataFrame(σ = Float64[],
@@ -53,13 +53,12 @@ for σ in σ_params
         get_market_balance(bank_sys)
         adjust_imbalance!(bank_sys)
         try
-            fund_matching!(bank_sys, 0.5)    
+            fund_matching!(bank_sys, 0.2)    
         catch
-            println("NO A_ib solution!!")
+            @warn "NO fund_matching solution!"
             continue
         end
         
-
         res_sim = DataFrame(σ = [σ],
                             n_default = [0],
                             degree = [degree(bank_sys)],

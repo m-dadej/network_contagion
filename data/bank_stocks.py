5f60ac3c491458c6c25c5fe43d9ecbf971d69d64
SquintRook <mateuszdadej@gmail.com>
2023-12-19 19:18:58 +0100

    CREDIT SPREAD

--- data/bank_stocks.py
+++ data/bank_stocks.py
@@ -3,6 +3,14 @@ import yfinance as yf
 import polars as pl
 import numpy as np
 import matplotlib.pyplot as plt
+from fredapi import Fred
+
+# HERE PASTE THE API KEY FROM FREDAPI
+fred = Fred(api_key='')
+spread = fred.get_series('BAMLC0A0CM')
+spread = pd.DataFrame(spread)
+spread.columns = ['spread']
+spread['spread_ch'] = spread['spread'].pct_change()
 
 tickers = "EBO.DE RAW.DE KBC.BR CBK.DE DBK.DE NDA-SE.ST DANSKE.CO JYSK.CO SYDB.CO BBVA BKT.MC CABK.MC SAB.MC SAN.MC UNI.MC BNP.PA ACA.PA GLE.PA ALPHA.AT EUROB.AT ETE.AT TPEIR.AT OTP.BD A5G.IR BARC.L BIRG.IR BAMI.MI ISP.MI MB.MI BMPS.MI BPE.MI UCG.MI ABN.AS INGA.AS DNB.OL PKO.WA PEO.WA BCP.LS SEB-A.ST SHB-A.ST SWED-A.ST"
 
@@ -18,7 +26,7 @@ bank_indx = df_rets.apply(lambda x: x.mean(), axis=1)
 
 cor_ts = df_rets\
     .fillna(0)\
-    .rolling(200, min_periods = 100)\
+    .rolling(100, min_periods = 100)\
     .corr()\
     .groupby(level='Date')\
     .mean()\
@@ -29,9 +37,11 @@ cor_ts.columns = ['cor']
 bank_indx = pd.DataFrame(bank_indx)
 bank_indx.columns = ['bank_indx']
 
-
-
-df_rets.join(cor_ts).join(bank_indx).to_csv("data/bank_cor.csv") 
+df_rets\
+    .join(cor_ts)\
+    .join(bank_indx)\
+    .join(spread)\
+    .to_csv("data/bank_cor.csv") 
 
 # robust yet fragile
 
